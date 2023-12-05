2d370ac30e5df0cfd175c4754df32f6e0d93322c
Maxime DAFFIS <m.daffis@criteo.com>
2023-12-05 14:58:18 +0100

    day 5 complete
    No optimization, this is dirty but hey it works

--- 2023/05/1/main.go
+++ 2023/05/1/main.go
@@ -16,81 +16,104 @@ func check(e error) {
 
 type Map struct {
 	label   string
-	seedMap map[int]int
+	seedMap []Route
 }
 
-func (g Map) getNextLocation(start int) int {
-	return g.seedMap[start]
+type Route struct {
+	source      int
+	destination int
+	rangeSize   int
 }
 
-func processMapLine(ranges []string, inProgressMap map[int]int) map[int]int {
-	origin, err := strconv.Atoi(ranges[0])
+func processRoute(ranges []string) Route {
+	origin, err := strconv.Atoi(ranges[1])
 	check(err)
 
-	destination, err := strconv.Atoi(ranges[1])
+	destination, err := strconv.Atoi(ranges[0])
 	check(err)
 
-	inProgressMap[origin] = destination
+	rangeSize, err := strconv.Atoi(ranges[2])
+	check(err)
 
-	// bad idea
-	// rangeSize, err := strconv.Atoi(ranges[2])
-	// check(err)
-	//
-	// for i := 0; i < rangeSize; i++ {
-	// 	inProgressMap[origin+i] = destination+i
-	// }
-
-	return inProgressMap
+	return Route{origin, destination, rangeSize}
 }
 
 func parseMaps(file *os.File) []Map {
 	fileScanner := bufio.NewScanner(file)
-	result := []Map{}
-	var newMap = Map{}
+	maps := []Map{}
+	newMap := Map{}
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 		var split = strings.Fields(line)
-		fmt.Println(len(split))
 		// start of a map
 		if len(split) == 2 {
 			newMap.label = split[0]
-			newMap.seedMap = make(map[int]int)
-
+			newMap.seedMap = []Route{}
 		}
 		if len(split) == 3 {
-			newMap.seedMap = processMapLine(split, newMap.seedMap)
+			newMap.seedMap = append(newMap.seedMap, processRoute(split))
 		}
 		// End of a map
 		if len(split) == 0 {
-			result = append(result, newMap)
+			maps = append(maps, newMap)
 			newMap = Map{}
 		}
 
-		fmt.Println(split)
 	}
 	file.Close()
-	return result
+	for _, currentMap := range maps {
+		for i := 0; i < len(currentMap.seedMap); i++ {
+			for j := i + 1; j < len(currentMap.seedMap); j++ {
+				if currentMap.seedMap[i].source > currentMap.seedMap[j].source {
+					currentMap.seedMap[i], currentMap.seedMap[j] = currentMap.seedMap[j], currentMap.seedMap[i]
+				}
+			}
+		}
+	}
+	return maps
 }
 
 func browseMaps(maps []Map, seed string) int {
-intseed, err := strconv.Atoi(seed)
-check(err)
-location := 0
-// check si seed est entre start et start + range et si oui on return seed - start + destination
+	seedInt, err := strconv.Atoi(seed)
+	check(err)
+
 	for _, currentMap := range maps {
-
-
+		for _, currentRoute := range currentMap.seedMap {
+			if seedInt >= currentRoute.source && seedInt < currentRoute.source+currentRoute.rangeSize {
+				seedInt = (seedInt - currentRoute.source) + currentRoute.destination
+				break
+			}
+		}
 
 	}
 
+	return seedInt
+}
+
+func parseSeeds([]string) []int {
+	return []int{}
 }
 
 func main() {
 	seeds := strings.Fields("1187290020 247767461 40283135 64738286 2044483296 66221787 1777809491 103070898 108732160 261552692 3810626561 257826205 3045614911 65672948 744199732 300163578 3438684365 82800966 2808575117 229295075")
-	fmt.Println(seeds)
 	f, err := os.Open("../data.txt")
 	check(err)
 	result := []Map{}
 	result = parseMaps(f)
-	fmt.Println(result)
+	location := []int{}
+	for _, seed := range seeds {
+		end := browseMaps(result, seed)
+		location = append(location, end)
+	}
+
+	//sort location
+	for i := 0; i < len(location); i++ {
+		for j := i + 1; j < len(location); j++ {
+			if location[i] > location[j] {
+				location[i], location[j] = location[j], location[i]
+			}
+		}
+	}
+
+	fmt.Println(location[0])
 }
---
+++ 2023/05/2/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Println(e)
+	}
+}
+
+type Map struct {
+	label   string
+	seedMap []Route
+}
+
+type Route struct {
+	source      int
+	destination int
+	rangeSize   int
+}
+
+type Seed struct {
+	start     int
+	rangeSize int
+}
+
+func processRoute(ranges []string) Route {
+	origin, err := strconv.Atoi(ranges[1])
+	check(err)
+
+	destination, err := strconv.Atoi(ranges[0])
+	check(err)
+
+	rangeSize, err := strconv.Atoi(ranges[2])
+	check(err)
+
+	return Route{origin, destination, rangeSize}
+}
+
+func parseMaps(file *os.File) []Map {
+	fileScanner := bufio.NewScanner(file)
+	maps := []Map{}
+	newMap := Map{}
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		var split = strings.Fields(line)
+		// start of a map
+		if len(split) == 2 {
+			newMap.label = split[0]
+			newMap.seedMap = []Route{}
+		}
+		if len(split) == 3 {
+			newMap.seedMap = append(newMap.seedMap, processRoute(split))
+		}
+		// End of a map
+		if len(split) == 0 {
+			maps = append(maps, newMap)
+			newMap = Map{}
+		}
+
+	}
+	file.Close()
+	for _, currentMap := range maps {
+		for i := 0; i < len(currentMap.seedMap); i++ {
+			for j := i + 1; j < len(currentMap.seedMap); j++ {
+				if currentMap.seedMap[i].source > currentMap.seedMap[j].source {
+					currentMap.seedMap[i], currentMap.seedMap[j] = currentMap.seedMap[j], currentMap.seedMap[i]
+				}
+			}
+		}
+	}
+	return maps
+}
+
+func browseMaps(maps []Map, seed Seed) int {
+	seedInt := seed.start
+	if seed.start == 82 {
+		fmt.Println("seedInt", seedInt)
+	}
+	for _, currentMap := range maps {
+
+		if seed.start == 82 {
+			fmt.Println("currentMap", currentMap.label)
+		}
+		for _, currentRoute := range currentMap.seedMap {
+			if seed.start == 82 {
+				fmt.Println(currentRoute)
+			}
+			if seedInt >= currentRoute.source && seedInt < currentRoute.source+currentRoute.rangeSize {
+				if seed.start == 82 {
+					fmt.Println(seedInt)
+					fmt.Println("=>")
+				}
+				seedInt = (seedInt - currentRoute.source) + currentRoute.destination
+				if seed.start == 82 {
+					fmt.Println(seedInt)
+				}
+				break
+			}
+		}
+	}
+
+	return seedInt
+}
+
+func parseSeeds(seedsString string) []Seed {
+	ints := strings.Fields(seedsString)
+	result := []Seed{}
+
+	for i := 0; i < len(ints); i = i + 2 {
+		parsedSeed, err := strconv.Atoi(ints[i])
+		check(err)
+		parsedRange, err := strconv.Atoi(ints[i+1])
+		newSeed := Seed{parsedSeed, parsedRange}
+		result = append(result, newSeed)
+	}
+	return result
+}
+
+func main() {
+	seeds := parseSeeds("1187290020 247767461 40283135 64738286 2044483296 66221787 1777809491 103070898 108732160 261552692 3810626561 257826205 3045614911 65672948 744199732 300163578 3438684365 82800966 2808575117 229295075")
+	f, err := os.Open("../data.txt")
+	check(err)
+	result := []Map{}
+	result = parseMaps(f)
+	lowestLocation := 0
+
+	for _, seed := range seeds {
+		fmt.Println(seed)
+		fmt.Println(seed.start, " ... ", seed.start+seed.rangeSize-1)
+		lowestLocation = browseMaps(result, seed)
+		for i := seed.start; i < seed.start+seed.rangeSize-1; i++ {
+			newSeed := Seed{i, 0}
+			end := browseMaps(result, newSeed)
+			if end < lowestLocation {
+				lowestLocation = end
+			}
+		}
+		fmt.Println(lowestLocation)
+
+	}
+
+}
