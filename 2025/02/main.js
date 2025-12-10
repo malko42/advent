b8c677aa72bbf1b3609b091b96e414c57e8800d2
Maxime Daffis <maxime.daffis@gmail.com>
2025-12-10 15:34:21 +0100

    [2025] day 2
    still node

--- 2025/02/main.js
+++ 2025/02/main.js
@@ -6,19 +6,84 @@ async function load_data() {
 }
 
 function parse_data(data) {
-  // split by newline, discard empty lines
-  const lines = data.split('\n').map(line => line.trim()).filter(line => line.length > 0);
-  console.log(`Loaded ${lines.length} lines of data.`);
-  return lines.map(line => {
-    let line_array = Array.from(line);
-    return line;
+  // split by comma
+  const ranges = data.split(',').map(range => range.trim()).map(range => {
+    const tmp = range.split('-');
+    const result = { start: tmp[0], end: tmp[1] };
+    if (isNaN(parseInt(result.start)) || isNaN(parseInt(result.end))) {
+      throw new Error(`Invalid range: ${range}`);
+    }
+    if (parseInt(result.start) > parseInt(result.end)) {
+      throw new Error(`Start greater than end: ${range}`);
+    }
+    return result;
   });
+  console.log(`Loaded ${ranges.length} ranges.`);
+  return ranges;
 }
 
-function process_1(lines) {
+function is_invalid(number) {
+  // number is a string
+  if (number.length % 2 !== 0) {
+    return false;
+  }
+
+  const left = number.slice(0, number.length / 2);
+  const right = number.slice(number.length / 2);
+
+  if (parseInt(left) == parseInt(right)) {
+    return true;
+  }
+
+  return false;
 }
 
-function process_2(lines) {
+function is_repeating(number) {
+  if (number.length < 2) {
+    return false;
+  }
+  const half = Math.floor(number.length / 2);
+  const left = number.slice(0, half);
+  const right = number.slice(half);
+  if (left == right) {
+    return true;
+  }
+
+  for (let i = half; i > 0; i--) {
+    const reg = new RegExp(`.{1,${i}}`, 'g');
+    const chunks = number.match(reg);
+    const test = chunks.every((val, _, arr) => val === arr[0]);
+    if (test) {
+      return true;
+    }
+  }
+  return false;
+}
+function process_1(ranges) {
+
+  let sum = 0;
+  for (const range of ranges) {
+    for (let i = parseInt(range.start); i <= parseInt(range.end); i++) {
+      if (is_invalid(i.toString())) {
+        sum += i;
+      }
+    }
+  }
+  return sum;
+}
+
+function process_2(ranges) {
+  let sum = 0;
+  for (const range of ranges) {
+    console.log(`Processing range: ${range.start}-${range.end}`);
+    for (let i = parseInt(range.start); i <= parseInt(range.end); i++) {
+      if (is_repeating(i.toString())) {
+        console.log(`Found repeating number: ${i}`);
+        sum += i;
+      }
+    }
+  }
+  return sum;
 }
 
 async function main() {
